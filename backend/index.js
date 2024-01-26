f4618c1b2a29e3dd7d94e6230f4f49eceb03c8ee
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2024-01-27 00:15:07 +0530

    add user and mainRouter

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,12 @@
 import express from 'express';
+import mainRouter from './routes/index.js';
+import userRouter from './routes/user.js';
 import { User } from './db.js';
 
 const app = express();
+app.use('/api/v1', mainRouter);
+app.use('/api/v1/user', userRouter);
 app.use(express.json());
-
-app.get('/', async (req, res) => {
-	const users = await User.find({ name: 'saad' });
-	res.status(200).json({
-		message: 'Hello from server.js',
-		users,
-	});
-});
-
-app.post('/signup', (req, res) => {
-	const { firstName, lastName, email, password } = req.body;
-	const user = new User({
-		firstName,
-		lastName,
-		email,
-		password,
-	});
-
-	user.save();
-	res.status(200).json({ message: 'User created successfully!' });
-});
 
 app.listen(5000, () => {
 	console.log('App listening on port 5000!');
