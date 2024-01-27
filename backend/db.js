d808a5563ab1bf1a65cccc161ee7fb9b1e1a69ed
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2024-01-27 23:42:27 +0530

    beautify userSchema

--- backend/db.js
+++ backend/db.js
@@ -17,10 +17,32 @@ mongoose
 	});
 
 const userSchema = new mongoose.Schema({
-	firstName: String,
-	lastName: String,
-	email: String,
-	password: String,
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		lowercase: true,
+		minLength: 3,
+		maxLength: 30,
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 6,
+	},
+	firstName: {
+		type: String,
+		required: true,
+		trim: true,
+		maxLength: 50,
+	},
+	lastName: {
+		type: String,
+		required: true,
+		trim: true,
+		maxLength: 50,
+	},
 });
 
 const accoutSchema = new mongoose.Schema({
