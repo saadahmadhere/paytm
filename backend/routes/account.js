0e3d54e4eac6b329fdecb602bec04fc33345ff78
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2024-03-24 05:25:48 +0530

    feat: get account balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import { authMiddleware } from '../middleware';
+import { Account } from '../models/accountModel';
 
 const router = express.Router();
+
+router.get('/', authMiddleware, async (req, res) => {
+	const userId = req.userId;
+
+	const account = await Account.findOne({ userId });
+	res.status(200).json({ balance: account.balance });
+});
 
 export default router;
